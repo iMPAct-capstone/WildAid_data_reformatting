944ce8b74b1f9a83df4696b59e55821ad62f41b2
adelaiderobinson <adelaide.robinson445@gmail.com>
2023-05-16 20:45:44 -0700

    finished validating correct number of rows for each year

--- src/individual_tests.R
+++ src/individual_tests.R
@@ -1,3 +1,27 @@
+#test that 2019 files are the correct length 
+if (unique(final_data$year) == 2019) {
+  
+  test_that("file_number", {
+    expect_true(nrow(final_data) == 31)
+  }) }
+
+
+#test that 2020 files are the correct length
+if (unique(final_data$year) == 2020) {
+
+  test_that("file_number", {
+    expect_true(nrow(final_data) == 31 | nrow(final_data) == 26 | nrow(final_data) == 5)
+  }) }
+
+
+#test that 2021 files are the correct length 
+if (unique(final_data$year) == 2021) {
+  
+  test_that("file_number", {
+    expect_true(nrow(final_data) == 27)
+  }) }
+
+#test that 2022 files are the correct length
 if (unique(final_data$year) == 2022) {
   
   test_that("file_number", {
